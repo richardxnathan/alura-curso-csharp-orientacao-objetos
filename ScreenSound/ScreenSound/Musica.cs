17ae460243f09340920c76a80b0094bb8fa66202
richardxnathan <richardnathand@gmail.com>
2023-12-19 11:21:24 -0300

    Aula 01

---
+++ ScreenSound/ScreenSound/Musica.cs
@@ -0,0 +1,29 @@
+﻿
+class Musica
+{
+    public string nome;
+    public string artista;
+    public int duracao;
+    public bool disponivel;
+
+    public void ExibirFichaTecnica()
+    {
+        Console.WriteLine($"Nome: {nome}");
+        Console.WriteLine($"Artista: {artista}");
+        Console.WriteLine($"Duração: {duracao}");
+        if (disponivel == true)
+        {
+            Console.WriteLine("Disponível no plano.");
+        } 
+        else 
+        {
+            Console.WriteLine("Adquira o plano premium para ter acesso a música.");
+        }
+    }
+    public void ExibirNomeEArtista()
+    {
+        Console.WriteLine($"Nome/Artista: {nome} - {artista}");
+    }
+
+}
+
---
+++ ScreenSound/ScreenSound/Program.cs
@@ -0,0 +1,17 @@
+﻿Musica primeiraMusica = new Musica();
+
+primeiraMusica.nome = "Mulher de Fases";
+primeiraMusica.artista = "Raimundos";
+primeiraMusica.duracao = 180;
+primeiraMusica.disponivel = true;
+
+primeiraMusica.ExibirFichaTecnica();
+
+Musica segundaMusica = new Musica();
+segundaMusica.nome = "Disorder";
+segundaMusica.artista = "Joy Division";
+segundaMusica.duracao = 165;
+segundaMusica.disponivel = false;
+
+segundaMusica.ExibirFichaTecnica();
+
